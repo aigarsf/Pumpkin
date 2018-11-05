365d8f6a6a42e48e1d8e436160ac535b99b2ce16
Aigars Fuksis <aigarsf@gmail.com>
2018-11-05 19:21:35 +0200

    SQL script to fill Blogging tables with dummy data added. MVC controller for Blogs section added. WebAPI controller for Blogs CRUD operations added.

---
+++ Pumpkin/SQL/Fill_Blogging_Tables.sql
@@ -0,0 +1,11 @@
+﻿insert into dbo.Blogs (Name)
+values
+('Aigars blog'),
+('Kristaps blog');
+
+insert into dbo.Posts (Title, [Content], BlogId)
+values
+('Aigars 1st post', 'lorem ipsum', 1),
+('Aigars 2nd post', 'dolor sit amet', 1),
+('Kristaps 1st post', 'the quick brown fox', 2),
+('Kristaps 2st post', 'jumps over the lazy dog', 2);
